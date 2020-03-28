b53652d7580fb3b6b81cb91e66b9b3ddad0baed8
Paulo Italo <pauloitalo@hotmail.com.br>
2020-03-28 19:44:37 -0300

    add Tabela On Vue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import './plugins/vue-bootstrap'
+import './style.scss'
 Vue.config.productionTip = false
 
 new Vue({
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/'
-    : '/'
+    : '/',
+  css: {
+    loaderOptions: {
+      scss: {
+      }
+    }
+  }
 }
