14593f768c4168c1169d5a24fb63a3358ed663d9
Paulo Italo <pauloitalo@hotmail.com.br>
2020-03-30 10:31:59 -0300

    add history mode express

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
-app = express()
-app.use(serveStatic(__dirname + '/dist'))
-var port = process.env.PORT || 5000
-app.listen(port)
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+var history = require('connect-history-api-fallback');
+var app = express();
+app.use(serveStatic(__dirname + '/dist'));
+app.use(history());
+var port = process.env.PORT || 5000;
+app.listen(port);
