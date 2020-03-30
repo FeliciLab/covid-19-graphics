eae3ae818194ff815934fa9aa6d18f334551c94b
Paulo Italo <pauloitalo@hotmail.com.br>
2020-03-30 10:10:50 -0300

    add taxa de óbitos na curva epidemiológica

--- src/components/CurvaEpidemiologica.vue
+++ src/components/CurvaEpidemiologica.vue
@@ -19,24 +19,31 @@ export default {
     const result = []
     response.data.reduce((res, value) => {
       if (!res[value.data]) {
-        res[value.data] = { x: value.data, y: 0 }
+        res[value.data] = { data: value.data, quantidade: 0, obitos: 0 }
         result.push(res[value.data])
       }
-      res[value.data].y += value.quantidade
+      res[value.data].quantidade += value.quantidade
+      res[value.data].obitos += value.obitos
       return res
     }, {})
 
     this.data = {
       labels: result.map(item => {
-        const splitted = item.x.split('-')
+        const splitted = item.data.split('-')
         return `${splitted[2]}/${splitted[1]}`
       }),
       datasets: [
         {
           label: 'Número de Casos Confirmados',
-          data: result.map(item => item.y),
+          data: result.map(item => item.quantidade),
           backgroundColor: 'rgba(52, 152, 219, 0.2)',
           borderColor: 'rgb(52, 152, 219)'
+        },
+        {
+          label: 'Número de óbitos',
+          data: result.map(item => item.obitos),
+          backgroundColor: 'rgba(250, 184, 34, 0.2)',
+          borderColor: 'rgb(250,184,34)'
         }
       ]
     }
