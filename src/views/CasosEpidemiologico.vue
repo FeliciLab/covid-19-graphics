cfbabd22480260df25e190400b214b88eb420540
Paulo Italo <pauloitalo@hotmail.com.br>
2020-04-01 09:57:18 -0300

    add Pagina de Casos Epidemiológicos

---
+++ src/views/CasosEpidemiologico.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <CasosPorDia>
+        <Curva>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
+<script>
+import CasosPorDia from '@/views/CasosPorDia'
+import Curva from '@/views/Curva'
+
+export default {
+  components: {
+    CasosPorDia,
+    Curva
+  }
+}
+</script>
