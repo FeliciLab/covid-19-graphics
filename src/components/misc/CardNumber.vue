c995736cd34de18c99eef3681dae71d5b4e7f9d8
Paulo Italo <pauloitalo@hotmail.com.br>
2020-04-01 16:18:56 -0300

    add CardNumber Component

---
+++ src/components/misc/CardNumber.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <img class="responsive" src="" alt=""/>
+        <p class="number text-center">{{ number }}</p>
+        <p class="text-center">{{ text }}</p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    // .number {
+
+    // }
+</style>
+
+<script>
+export default {
+  props: ['number', 'text', 'typeOfCard']
+}
+</script>
